84905988644d57e5024541ea073bc6f0a04ef949
GeorgeProjects <liguozhengsdu@gmail.com>
2021-01-05 10:31:14 +0800

    processing5:         # self.filter_consecutives()

--- deepsvg/svglib/svg.py
+++ deepsvg/svglib/svg.py
@@ -337,7 +337,7 @@ class SVG:
             self.normalize()
 
         # self.split_paths()
-        self.filter_consecutives()
+        # self.filter_consecutives()
         self.filter_empty()
         self._apply_to_paths("reorder")
         # self.svg_path_groups = sorted(self.svg_path_groups, key=lambda x: x.start_pos.tolist()[::-1])
