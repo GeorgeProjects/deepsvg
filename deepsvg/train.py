b579c2048a6872f85d8f4feef225154e8e33e28f
GeorgeProjects <liguozhengsdu@gmail.com>
2021-01-06 10:40:22 +0800

    change max_num_groups parameter

--- deepsvg/train.py
+++ deepsvg/train.py
@@ -19,6 +19,7 @@ def train(cfg: _Config, model_name, experiment_name="", log_dir="./logs", debug=
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     cfg.batch_size = 5
+    cfg.max_num_groups = 15
     print('cfg.batch_size', cfg.batch_size)
 
     print("Parameters")
